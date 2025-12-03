3019a40434595f9f2fec138a46171a1ebe747aed
Tydedev <basso.maria1@outlook.it>
2025-12-03 09:38:46 +0100

    added params to og

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -8,7 +8,11 @@ export const alt = "Tydedev";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
-export default async function Image() {
+type Props = {
+  params: Promise<{ locale: string }>;
+};
+
+export default async function Image({ params }: Props) {
   const interBold = await readFile(
     path.resolve(join(process.cwd(), "assets/fonts/Inter-Bold.ttf"))
   );
