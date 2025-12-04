570f5e9f4f63e6d3cbca864c6c7c1835e414e2fa
Tydedev <basso.maria1@outlook.it>
2025-12-04 10:06:53 +0100

    new sitemap 1.0

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,32 +2,28 @@
 const siteUrl = "https://tydedev.it";
 const locales = ["it", "en"];
 
+const staticPages = ["", "contacts", "privacy"]; // tutte le pagine statiche
+
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
-  transform: async (config, path) => {
-    const pathLocale = path.split("/")[1]; // 'it' o 'en'
-    const alternateRefs = locales.map((l) => ({
-      href: `${siteUrl}/${l}${path.replace(`/${pathLocale}`, "")}`,
-      hreflang: l,
-    }));
-    return {
-      loc: `${siteUrl}${path}`,
-      changefreq: "daily",
-      priority: 0.7,
-      alternateRefs,
-    };
-  },
   additionalPaths: async () => {
-    // Lista tutte le pagine statiche che non vengono trovate automaticamente
     const paths = [];
     locales.forEach((locale) => {
-      paths.push(`/${locale}`);
-      paths.push(`/${locale}/contacts`);
-      paths.push(`/${locale}/privacy`);
-      // aggiungi altre pagine statiche se vuoi
+      staticPages.forEach((page) => {
+        const loc = page ? `/${locale}/${page}` : `/${locale}`;
+        paths.push({
+          loc,
+          changefreq: "daily",
+          priority: 0.7,
+          alternateRefs: locales.map((l) => ({
+            href: `${siteUrl}/${l}${page ? "/" + page : ""}`,
+            hreflang: l,
+          })),
+        });
+      });
     });
     return paths;
   },
