14bbb80c259dc5f26880194e23ec444168e6dcec
Tydedev <basso.maria1@outlook.it>
2025-12-04 10:46:14 +0100

    simple sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,44 +1,6 @@
-
-const siteUrl = "https://tydedev.it";
-const locales = ["it", "en"];
-const pages = ["", "contacts", "privacy"]; // le tue pagine principali
-
-const config = {
-  siteUrl,
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: "daily",
-  priority: 0.7,
-  autoLastmod: true,
-
-  // Trasforma ogni path aggiungendo gli alternateRefs
-  transform: async (config, path) => {
-    return {
-      loc: `${siteUrl}${path}`,
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: locales.map((locale) => ({
-        href: `${siteUrl}/${locale}${path}`,
-        hreflang: locale,
-      })),
-    };
-  },
-
-  // Percorsi extra
-  additionalPaths: async (config) => {
-    const paths = [];
-    for (const page of pages) {
-      for (const locale of locales) {
-        paths.push(await config.transform(config, `/${page ? page : ""}`));
-      }
-    }
-    return paths;
-  },
-
-  robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-  },
-};
-
-export default config;
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://tydedev.it',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
