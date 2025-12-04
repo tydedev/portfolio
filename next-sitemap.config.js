4c070b1e0e48efd87c33cf726daa2e506c474d1e
Tydedev <basso.maria1@outlook.it>
2025-12-04 09:59:41 +0100

    improved multilingual sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,57 @@
-const locales = ["it", "en"];
+const siteUrl = "https://tydedev.it";
 
 module.exports = {
-  siteUrl: "https://tydedev.it",
+  siteUrl,
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
-  generateI18nRoutes: true,
+
+  // Le lingue del sito
+  alternateRefs: [
+    { href: `${siteUrl}/it`, hreflang: "it" },
+    { href: `${siteUrl}/en`, hreflang: "en" },
+    { href: `${siteUrl}/it`, hreflang: "x-default" }, // fallback
+  ],
+
+  // Pagine extra da includere
   additionalPaths: async (config) => {
-    const paths = [];
-    locales.forEach(locale => {
-      paths.push({ loc: `/${locale}` });
-      paths.push({ loc: `/${locale}/contacts` });
-    });
-    return paths;  
+    return [
+      {
+        loc: "/it",
+        changefreq: "daily",
+        priority: 0.7,
+        alternateRefs: [
+          { href: `${siteUrl}/it`, hreflang: "it" },
+          { href: `${siteUrl}/en`, hreflang: "en" },
+        ],
+      },
+      {
+        loc: "/en",
+        changefreq: "daily",
+        priority: 0.7,
+        alternateRefs: [
+          { href: `${siteUrl}/it`, hreflang: "it" },
+          { href: `${siteUrl}/en`, hreflang: "en" },
+        ],
+      },
+      {
+        loc: "/it/contacts",
+        changefreq: "daily",
+        priority: 0.7,
+        alternateRefs: [
+          { href: `${siteUrl}/it/contacts`, hreflang: "it" },
+          { href: `${siteUrl}/en/contacts`, hreflang: "en" },
+        ],
+      },
+      {
+        loc: "/en/contacts",
+        changefreq: "daily",
+        priority: 0.7,
+        alternateRefs: [
+          { href: `${siteUrl}/it/contacts`, hreflang: "it" },
+          { href: `${siteUrl}/en/contacts`, hreflang: "en" },
+        ],
+      },
+    ];
   },
 };
