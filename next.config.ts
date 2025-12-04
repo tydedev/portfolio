396aa43dbdb91e9a6a2ffeedc8b725b9619e8d45
Tydedev <basso.maria1@outlook.it>
2025-12-04 11:00:21 +0100

    edited next config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  i18n: {
+    locales: ["en", "it"], // add all languages you support
+    defaultLocale: "en",
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
