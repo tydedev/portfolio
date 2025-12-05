16a12550e9ab8adcd744423ddb3ff439264d4c40
Tydedev <basso.maria1@outlook.it>
2025-12-05 19:56:26 +0100

    edited menus

--- components/common/MobileNav.tsx
+++ components/common/MobileNav.tsx
@@ -13,15 +13,19 @@ import {
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import type { LucideIcon } from "lucide-react";
+import { DropdownMenuSeparator } from "../ui/dropdown-menu";
 
 type LinkItem = {
   name: string;
   href?: string;
   icon?: LucideIcon;
+  isDropDown?: boolean;
+  dropDown?: { name: string; href?: string }[];
 };
 
 const MobileNav = ({ links }: { links: LinkItem[] }) => {
   const t = useTranslations("header.nav");
+  const s = useTranslations("services");
   const [open, setOpen] = useState(false);
 
   return (
@@ -41,24 +45,44 @@ const MobileNav = ({ links }: { links: LinkItem[] }) => {
 
             <div className="p-6 flex flex-col gap-2">
               {links.map((link) => {
-                const Icon = link.icon;
-                return (
-                  <Button
-                    key={link.name}
-                    variant="ghost"
-                    size="sm"
-                    className="text-base justify-start font-semibold leading-6 text-muted-foreground hover:text-foreground"
-                    asChild
-                  >
-                    <Link
-                      href={link.href || "/"}
+                if (!link.dropDown) {
+                  return (
+                    <Button
                       onClick={() => setOpen(false)}
+                      key={link.name}
+                      variant="ghost"
+                      size="sm"
+                      className="text-sm font-semibold leading-6 text-muted-foreground hover:text-foreground"
+                      asChild
                     >
-                      {Icon && <Icon className="mr-2 h-4 w-4" />}
-                      {t(link.name)}
-                    </Link>
-                  </Button>
-                );
+                      <Link href={link.href || ""}>{t(link.name)}</Link>
+                    </Button>
+                  );
+                }
+              })}
+              <DropdownMenuSeparator className="my-3" />
+              {links.map((link) => {
+                if (link.dropDown) {
+                  return (
+                    <div key={link.name} className="w-full">
+                      <p className="text-center mb-6">{t(link.name)}</p>
+                      <div className="flex flex-col gap-2 w-full">
+                        {link.dropDown.map((item) => (
+                          <Button
+                            onClick={() => setOpen(false)}
+                            key={item.name}
+                            variant="ghost"
+                            size="sm"
+                            className="text-sm font-semibold leading-6 text-muted-foreground hover:text-foreground"
+                            asChild
+                          >
+                            <Link href={item.href || ""}>{s(item.name)}</Link>
+                          </Button>
+                        ))}
+                      </div>
+                    </div>
+                  );
+                }
               })}
             </div>
           </div>
