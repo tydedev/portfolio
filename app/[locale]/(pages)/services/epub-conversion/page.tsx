e700315c091f645cd31061bff28594b78ccf3668
tydedev <basso.maria1@outlook.it>
2026-05-07 17:27:44 +0200

    Added schema for service

--- app/[locale]/(pages)/services/epub-conversion/page.tsx
+++ app/[locale]/(pages)/services/epub-conversion/page.tsx
@@ -5,37 +5,74 @@ import Image from "next/image";
 export default function EpubPage() {
   const t = useTranslations("Services.EpubConversion");
 
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Service",
+    serviceType: t("type"),
+    name: t("title"),
+    description: t("description"),
+    provider: {
+      "@type": "Person",
+      name: "Tydedev",
+      url: "https://tydedev.it",
+    },
+    areaServed: {
+      "@type": "Country",
+      name: "Italy",
+    },
+    url: "https://tydedev.it/services/epub-conversion",
+    image: "https://tydedev.it/images/kindle.jpg",
+    offers: {
+      "@type": "Offer",
+      availability: "https://schema.org/InStock",
+    },
+  };
+
   return (
-    <section className="grid grid-cols-1 md:grid-cols-12 gap-6 max-w-300 mx-auto w-full my-20 md:my-40 px-4 md:px-0">
-      {/* INTRO */}
-      <div className="md:col-span-full mb-10 md:mb-20 col-span-full">
-        <h1 className="font-semibold text-base leading-relaxed">
-          {t("intro")}
-        </h1>
-      </div>
-      <div className="col-span-full md:col-span-3">
-        <p className="text-sm">{t("info1")}</p>
-      </div>
-      <div className="col-span-full md:col-span-3">
-        <p className="text-sm">{t("info2")}</p>
-      </div>
-      <div className="col-span-full row-span-2 md:row-start-2 md:col-start-8 relative overflow-hidden min-h-50">
-        <Image
-          src="/images/kindle.jpg"
-          alt="Epub"
-          fill
-          className="object-cover w-full h-auto"
-          unoptimized
-          priority
-        />
-      </div>
-      <div className="col-span-full md:col-span-3 md:col-start-4">
-        <p className="text-sm font-semibold">{t("ideal_for")}</p>
-      </div>
-      <div className="col-span-full md:col-span-3 font-semibold text-2xl md:text-3xl tracking-tight leading-none">
-        <p className="text-md">{t("contact")}</p>
-      </div>
-    </section>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd),
+        }}
+      />
+
+      <section className="grid grid-cols-1 md:grid-cols-12 gap-6 max-w-300 mx-auto w-full my-20 md:my-40 px-4 md:px-0">
+        {/* INTRO */}
+        <div className="md:col-span-full mb-10 md:mb-20 col-span-full">
+          <h1 className="font-semibold text-base leading-relaxed">
+            {t("intro")}
+          </h1>
+        </div>
+
+        <div className="col-span-full md:col-span-3">
+          <p className="text-sm">{t("info1")}</p>
+        </div>
+
+        <div className="col-span-full md:col-span-3">
+          <p className="text-sm">{t("info2")}</p>
+        </div>
+
+        <div className="col-span-full row-span-2 md:row-start-2 md:col-start-8 relative overflow-hidden min-h-50">
+          <Image
+            src="/images/kindle.jpg"
+            alt="Epub"
+            fill
+            className="object-cover w-full h-auto"
+            unoptimized
+            priority
+          />
+        </div>
+
+        <div className="col-span-full md:col-span-3 md:col-start-4">
+          <p className="text-sm font-semibold">{t("ideal_for")}</p>
+        </div>
+
+        <div className="col-span-full md:col-span-3 font-semibold text-2xl md:text-3xl tracking-tight leading-none">
+          <p className="text-md">{t("contact")}</p>
+        </div>
+      </section>
+    </>
   );
 }
 
@@ -52,7 +89,11 @@ export async function generateMetadata(
   return {
     title: t("intro"),
     description: t("description"),
-    canonical: `https://tydedev.it/${locale}/services/epub-conversion`,
+
+    alternates: {
+      canonical: `https://tydedev.it/${locale}/services/epub-conversion`,
+    },
+
     keywords: [
       "graphic design",
       "web development",
