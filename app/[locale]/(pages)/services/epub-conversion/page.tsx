2d538041f03717c5fac85903c387be631bec3097
tydedev <basso.maria1@outlook.it>
2026-05-08 15:14:13 +0200

    Added schema to ebook page

--- app/[locale]/(pages)/services/epub-conversion/page.tsx
+++ app/[locale]/(pages)/services/epub-conversion/page.tsx
@@ -1,42 +1,44 @@
 import { Locale, useTranslations } from "next-intl";
-import { getTranslations } from "next-intl/server";
+import { getLocale, getTranslations } from "next-intl/server";
+import Script from "next/script";
+import { Product, WithContext } from "schema-dts";
 import Image from "next/image";
+
+const locale = await getLocale();
+const t = await getTranslations({
+  locale: locale as Locale,
+  namespace: "Services.EpubConversion",
+});
+
+const jsonLd: WithContext<Product> = {
+  "@context": "https://schema.org",
+  "@type": "Product",
+  name: t("intro"),
+  description: t("description"),
+  brand: {
+    "@type": "Brand",
+    name: "Tydedev",
+  },
+  category: "Digital Publishing Services",
+  offers: {
+    "@type": "Offer",
+    availability: "https://schema.org/InStock",
+    url: "https://tydedev.it/services/epub-conversion",
+  },
+};
 
 export default function EpubPage() {
   const t = useTranslations("Services.EpubConversion");
 
-  const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "Service",
-    serviceType: t("type"),
-    name: t("title"),
-    description: t("description"),
-    provider: {
-      "@type": "Person",
-      name: "Tydedev",
-      url: "https://tydedev.it",
-    },
-    areaServed: {
-      "@type": "Country",
-      name: "Italy",
-    },
-    url: "https://tydedev.it/services/epub-conversion",
-    image: "https://tydedev.it/images/kindle.jpg",
-    offers: {
-      "@type": "Offer",
-      availability: "https://schema.org/InStock",
-    },
-  };
-
   return (
     <>
-      <script
+      <Script
+        id="faq-schema"
         type="application/ld+json"
         dangerouslySetInnerHTML={{
           __html: JSON.stringify(jsonLd),
         }}
       />
-
       <section className="grid grid-cols-1 md:grid-cols-12 gap-6 max-w-300 mx-auto w-full my-20 md:my-40 px-4 md:px-0">
         {/* INTRO */}
         <div className="md:col-span-full mb-10 md:mb-20 col-span-full">
